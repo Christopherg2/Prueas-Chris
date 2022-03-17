f119856133897a078ac6a453258bd91cc910ef8c
Christopherg2 <62117067+Christopherg2@users.noreply.github.com>
2022-03-16 21:40:16 -0600

    Nuevas librerias
    Nuevas librerias

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script>
-    <script src="https://libs.zappar.com/zappar-aframe/0.3.6/zappar-aframe.js"></script>
+  <head>
+    <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script>
+    <script src="https://libs.zappar.com/zappar-aframe/0.3.6/zappar-aframe.js"></script>
 
-    <title>RA de Git Hub Octacot</title>
-  </head>
-  <body>
-    <a-scene>
-      <a-entity zappar-permissions-ui id="permissions"></a-entity>
+    <title>RA de Git Hub(Octacat)</title>
+  </head>
+  <body>
+    <a-scene>
+      <a-entity zappar-permissions-ui id="permissions"></a-entity>
 
-      <a-entity zappar-compatibility-ui id="compatibility"></a-entity>
+      <a-entity zappar-compatibility-ui id="compatibility"></a-entity>
 
-      <a-camera zappar-camera></a-camera>
+      <a-camera zappar-camera></a-camera>
 
-      <a-entity
-        zappar-image="target:https://cdn.glitch.global/be8afb92-31e5-4eea-8c26-7f86a9ec9d19/marcador%20git.zpt?v=1647372512040"
-      >
-        <a-entity
-          id="model"
-          gltf-model="https://cdn.glitch.global/be8afb92-31e5-4eea-8c26-7f86a9ec9d19/OCTOCAT3.glb?v=1647374966190"
-        ></a-entity>
-      </a-entity>
-    </a-scene>
-  </body>
+      <a-entity
+        zappar-image="target:https://cdn.glitch.global/be8afb92-31e5-4eea-8c26-7f86a9ec9d19/marcador%20git.zpt?v=1647372512040"
+      >
+        <a-entity
+          id="model"
+          gltf-model="https://cdn.glitch.global/be8afb92-31e5-4eea-8c26-7f86a9ec9d19/OCTOCAT3.glb?v=1647374966190"
+        ></a-entity>
+      </a-entity>
+    </a-scene>
+  </body>
 </html>
